07c505fe99c18db1d532bf2c67e9da04f7eb6568
Selkief <s.kiefersauer@gmx.de>
2026-05-12 17:24:59 +0200

    minor fixes, add q function and ode solver

--- chemistry.py
+++ chemistry.py
@@ -10,23 +10,23 @@ from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
 #load data with all the densities
-msis = pd.read_csv("C:/Users/skief/Documents/UiT/semester8/space physics/progrTasks/1atmosphere/MSIS.dat",sep=r"\s+", skiprows=16)
+msis = pd.read_csv("atmosphere/MSIS.dat",sep=r"\s+", skiprows=17)
 iri = pd.read_csv("IRI.dat", sep=r"\s+", skiprows=45)
 
-n_e_data = iri.iloc[:,1].to_numpy()*1e6 #density in m^-3
+n_e_data = iri.iloc[:,1].to_numpy() #density in m^-3
 n_O_data = msis.iloc[100:,1].to_numpy()*1e6 #density in m^-3
-n_Oplus_data = iri.iloc[:,4].to_numpy()/100 * n_e_data #density m^-3
+frac_Oplus = iri.iloc[:,4].to_numpy()/100
+n_Oplus_data = frac_Oplus * n_e_data #density m^-3
 n_O2_data = msis.iloc[100:,3].to_numpy()*1e6 #density m^-3
-n_O2plus_data = iri.iloc[:,7].to_numpy()/100 * n_e_data #density m^-3
+frac_O2plus = iri.iloc[:,7].to_numpy()/100
+n_O2plus_data = frac_O2plus * n_e_data #density m^-3
 n_N2_data = msis.iloc[100:,2].to_numpy()*1e6 #density m^-3
-###dummy values bc i didnt find the values, must replace!!!!
-n_N2plus_data = np.zeros_like(n_N2_data)
-n_NOplus_data = iri.iloc[:,8].to_numpy()/100 * n_e_data #density m^-3
-###dummy values must replace!!!!
+frac_NOplus = iri.iloc[:,8].to_numpy()/100
+n_NOplus_data = frac_NOplus * n_e_data #density m^-3
+###dummy values
 n_NO_data = np.zeros_like(n_NOplus_data)
+n_N2plus_data = np.zeros_like(n_N2_data)
 
-##this equation is still wrong as all the n's should be q's
-q_N2plus = n_e_data * 0.92 * n_N2_data / (0.92 * n_N2_data + n_O2_data + 0.56 * n_O_data)
 
 #temperatures
 Te = iri.iloc[:,3].to_numpy()
@@ -40,7 +40,7 @@ alpha1 = 4.2e-13 * np.power((Te/300), -0.85)
 alpha2 = 1.9e-13 * np.power((Te/300), -0.5)
 alpha3 = 1.8e-13 * np.power((Te/300), -0.39)
 alphar = 3.7e-18 * np.power((Te/250), -0.7)
-k1 = 1.3e-18 * np.power((Tr/300),-0.5) + 6.8e-16 * np.exp(-1.4e-19*(kB*Tr))
+k1 = 1.3e-18 * np.power((Tr/300),-0.5) + 6.8e-16 * np.exp(-1.4e-19/(kB*Tr))
 k2 = 2e-17 * np.power((Tr/300), -0.4)
 k3 = 4.4e-16
 k4 = 5e-22
@@ -51,7 +51,8 @@ k6 = 5e-17 * np.power((Tr/300), -0.8)
 
 #define ODEs for a certain height 
 def initial_cond(height):
-    #initial densities for a certain height
+    #gets initial densities for a certain height from the arrays with the nr densities
+    #and returns them
     n_e = n_e_data[ height - 100 ]
     n_Oplus = n_Oplus_data[ height - 100]
     n_O2plus = n_O2plus_data[ height - 100]
@@ -59,17 +60,40 @@ def initial_cond(height):
     n_NO = n_NO_data[ height - 100]
     n_NOplus = n_NOplus_data[ height - 100]
 
-    return n_e, n_Oplus, n_O2plus, n_N2plus, n_NO, n_NOplus
+    return n_e, n_Oplus, n_O2plus, n_N2plus, n_NOplus, n_NO
+
+def calc_q(ionization_rate, height):
+    #calculates ionisation rate for all the ion species and the electrons from total ionization rate
+    #returns array with rate for [q_e, q_Oplus, q_O2plus, q_NOplus, q_NO]
+    idx = height-100
+    q_tot = ionization_rate
+    q_e = q_tot
+    q_Oplus = frac_Oplus[idx] * q_tot
+    q_O2plus = frac_O2plus[idx] * q_tot
+    q_NOplus = frac_NOplus[idx] * q_tot
+    q_N2plus = q_e * 0.92 * n_N2_data[idx] / (0.92 * n_N2_data[idx] + n_O2_data[idx] + 0.56 * n_O_data[idx])
+    q_NO = 0.0 #neutral, doesn't have ionization rate
+    
+    return [q_e, q_Oplus, q_O2plus, q_N2plus, q_NOplus, q_NO]
+
 
 def reactions(t, z, q, height):
+    #ODEs for coupled cont equations 
+    # that calculate production and loss of major species in atmosphere
+    #q is an array of all ionizatio rates, in same order as z
 
+    #load initial conditions defined in args of solve_ivp
     n_e, n_Oplus, n_O2plus, n_N2plus, n_NOplus, n_NO = z
+    q_e, q_Oplus, q_O2plus, q_N2plus, q_NOplus, q_NO = q
 
     idx = height - 100
+
+    #add neutral densities that we dont track in ODEs
     n_O = n_O_data[ idx ]
     n_O2 = n_O2_data[ idx ]
     n_N2 = n_N2_data[ idx]
 
+    #reaction rates, many are dependent on temperature which is dependent on height
     alpha_1 = alpha1[idx]
     alpha_2 = alpha2[idx]
     alpha_3 = alpha3[idx]
@@ -81,28 +105,50 @@ def reactions(t, z, q, height):
     k_5 = k5[idx]
     k_6 = k6[idx]
     
-    #set up ODEs !!!MUSt adjust ion_rates in equations?!!
-    dn_e = q - n_e * (alpha_1 * n_NOplus + alpha_2 * n_O2plus + alpha_3 * n_N2plus + alpha_r * n_Oplus)
-    dn_Oplus = q + k_5 * n_O * n_N2plus - n_Oplus * (alpha_r * n_e + k_1 * n_N2 + k_2 * n_O2)
-    dn_O2plus = q + k_2 * n_Oplus * n_O2 + k_6 * n_N2plus * n_O2 - n_O2plus * (alpha_2 * n_e + k_3 * n_NO + k_4 * n_N2)
-    dn_N2plus = q - n_N2plus * (alpha_3 * n_e + k_5 * n_O + k_6 * n_O2)
-    dn_NOplus = q + k_1 * n_Oplus * n_N2 + k_3 * n_O2plus * n_NO + k_4 * n_O2plus * n_N2 - alpha_1 * n_NOplus *n_e
-    dn_NO = q + k_4 * n_O2plus * n_N2 - k_3 * n_O2plus * n_NO
+    #set up ODEs
+    dn_e = q_e - n_e * (alpha_1 * n_NOplus + alpha_2 * n_O2plus + alpha_3 * n_N2plus + alpha_r * n_Oplus)
+    dn_Oplus = q_Oplus + k_5 * n_O * n_N2plus - n_Oplus * (alpha_r * n_e + k_1 * n_N2 + k_2 * n_O2)
+    dn_O2plus = q_O2plus + k_2 * n_Oplus * n_O2 + k_6 * n_N2plus * n_O2 - n_O2plus * (alpha_2 * n_e + k_3 * n_NO + k_4 * n_N2)
+    dn_N2plus = q_N2plus - n_N2plus * (alpha_3 * n_e + k_5 * n_O + k_6 * n_O2)
+    dn_NOplus = q_NOplus + k_1 * n_Oplus * n_N2 + k_3 * n_O2plus * n_NO + k_4 * n_O2plus * n_N2 - alpha_1 * n_NOplus *n_e
+    dn_NO = q_NO + k_4 * n_O2plus * n_N2 - k_3 * n_O2plus * n_NO
 
     return [dn_e, dn_Oplus, dn_O2plus, dn_N2plus, dn_NOplus, dn_NO]
 
-IC = initial_cond(110)
-sol = solve_ivp(reactions, [0,360], IC, method = "RK45", args=(1e9, 250))
 
+def ODE_solver(altitude):
+    #integrate for 3600s with constant ionizatio rate of 1e9 (height 110km)
+    q0 = calc_q(1e9, altitude)
+    IC = initial_cond(altitude)
+    sol0 = solve_ivp(reactions, [0,3600], IC, method = "RK45", args=(q0, altitude))
+
+    #use previous solution as new initial conditions
+    #integrate with q_e=2*1e10 for 160s, then no ionisation at all
+    q1 = calc_q(2e10, altitude)
+    sol1 = solve_ivp(reactions, [3600, 3600+160], sol0.y[:,-1], method="RK45", args=(q1, altitude) )
+    q2 = calc_q(0.0, altitude)
+    sol2 = solve_ivp(reactions, [3600+160, 3600+400], sol1.y[:,-1], method="RK45", args=(q2, altitude) )
+
+    sols = np.hstack((sol0.y, sol1.y, sol2.y))  # Combine solutions along the state variable axis
+    times = np.hstack((sol0.t, sol1.t, sol2.t))      #combine time arrays
+    return sols, times
+
+solutions_250, time_250 = ODE_solver(250)
+electrons = solutions_250[0,:]
+ions = np.sum(solutions_250[1:5,:], axis=0)
+charge = ions - electrons
 
 labels = ["e", "O+", "O2+", "N2+", "NO+", "NO"]
-fig, axs = plt.subplots(6,1, figsize=(18,18))
-for idx, ele in enumerate(sol.y):
-    axs[idx].plot(sol.t, ele.T, label = labels[idx])
-    axs[idx].set_xlabel("t")
-    #axs[idx].plot_legend()
-plt.suptitle("densities at height 110km")
+for idx, ele in enumerate(solutions_250):
+    plt.plot(time_250, ele[:], label = labels[idx])
+plt.xlabel("t")
+plt.legend()
+plt.suptitle("densities at height 250km")
 plt.tight_layout()
+plt.show()
+
+plt.plot(time_250, charge)
+plt.title("charge conservation")
 plt.show()
 
 
